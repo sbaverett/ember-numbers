e16f15dfea47fa697a70dbc6348e5ca7ae59d990
Sam Averett <averettsam@comcast.net>
2014-06-27 16:54:30 -0700

    added two new tests

--- test/app/test.js
+++ test/app/test.js
@@ -40,4 +40,38 @@ describe('app', function() {
       });
     });
   });
+    describe('when visiting the additions page', function() {
+    beforeEach(function() {
+      visit('/additions');
+    });
+
+    it.skip('lets the user save a new number', function() {
+      click('button[name="add"] a');
+      fillIn('input[name="numberName"]', 'three');
+      fillIn('input[name="anotherLanguage"]', 'tres');
+      fillIn('input[name="url"]', 'http://en.wikipedia.org/wiki/3_(number)');
+      click('button[name="save"] a');
+      andThen(function() {
+        expect(find('record[numberName="three"] a').to.exist);
+      });
+    });
+  });
+    describe('when visiting a numberPage', function() {
+    beforeEach(function() {
+      visit('/numbers');
+    });
+
+    it('lets the user delete a number', function() {
+      var buttonCount = (find('button a').length);
+      visit('/number/:id');
+      click('button[name="delete"] a');
+      andThen(function() {
+        expect(currentRouteName()).to.eql('numbers');
+        expect(currentPath()).to.eql('numbers');
+        expect(currentURL()).to.eql('/numbers');
+        expect(find('button a').length).to.eql(buttonCount - 1);
+      });
+    });
+  });
 });
+
