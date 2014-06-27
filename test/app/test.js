90b9a30e9d39f5e6f4dc43eedd5042bbaa8f0ae8
J. Allen Tucker <j.allentucker@gmail.com>
2014-06-27 10:45:22 -0700

    visit before each button clicking test

--- test/app/test.js
+++ test/app/test.js
@@ -1,22 +1,26 @@
 'use strict';
 
 describe('app', function() {
-  it('lets the user move from index to numbers', function() {
-    visit('/');
-    click('button[name="numbers"] a');
-    andThen(function() {
-      expect(currentRouteName()).to.eql('numbers');
-      expect(currentPath()).to.eql('numbers');
-      expect(currentURL()).to.eql('/numbers');
+  describe('when visiting the home page', function() {
+    beforeEach(function() {
+      visit('/');
     });
-  });
-  it('lets the user stay on index after clicking home button', function() {
-    visit('/');
-    click('button[name="home"] a');
-    andThen(function() {
-      expect(currentRouteName()).to.eql('index');
-      expect(currentPath()).to.eql('index');
-      expect(currentURL()).to.eql('/');
+
+    it('lets the user move from index to numbers', function() {
+      click('button[name="numbers"] a');
+      andThen(function() {
+        expect(currentRouteName()).to.eql('numbers');
+        expect(currentPath()).to.eql('numbers');
+        expect(currentURL()).to.eql('/numbers');
+      });
+    });
+    it('lets the user stay on index after clicking home button', function() {
+      click('button[name="home"] a');
+      andThen(function() {
+        expect(currentRouteName()).to.eql('index');
+        expect(currentPath()).to.eql('index');
+        expect(currentURL()).to.eql('/');
+      });
     });
   });
 });
